8d1f2e52cad9c82334e62a098dad5bd718c7732a
Nicholas Dongoske <nickdongoske@gmail.com>
2019-12-30 02:47:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,25 @@
-def square_array(array)
-  # your code here
+numbers = [1, 2, 3]
+def square_array(numbers)
+counter = 1
+  while counter <= numbers.length
+  puts counter ** 2
+  counter += 1
 end
+end
+
+
+
+
+
+
+
+# # numbers = [1, 2, 3]
+# def square_array(numbers)
+#   puts numbers.length ** 2
+# counter = 0
+# while counter < numbers.length ** 2
+# puts numbers 
+# end
+# end
+
+
